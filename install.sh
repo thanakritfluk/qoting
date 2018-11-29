13f90c4e0844f34cc319f8235f19b085fb648e62
Piyaphol Wiengperm <piyaphol.w@ku.th>
2018-11-29 13:18:37 +0700

    Fix selenium

--- install.sh
+++ install.sh
@@ -24,3 +24,5 @@ wget -N http://selenium-release.storage.googleapis.com/$SELENIUM_SUBDIR/selenium
 sudo mv -f ~/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar /usr/local/bin/selenium-server-standalone.jar
 sudo chown root:root /usr/local/bin/selenium-server-standalone.jar
 sudo chmod 0755 /usr/local/bin/selenium-server-standalone.jar
+
+xvfb-run java -Dwebdriver.chrome.driver=/usr/bin/chromedriver -jar selenium-server-standalone.jar
