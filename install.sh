f14d8e3a8a957a47d385ce0d5cafacde8ce3e375
Piyaphol Wiengperm <piyaphol.w@ku.th>
2018-11-29 13:27:15 +0700

    Fixed selenium

--- install.sh
+++ install.sh
@@ -24,5 +24,3 @@ wget -N http://selenium-release.storage.googleapis.com/$SELENIUM_SUBDIR/selenium
 sudo mv -f ~/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar /usr/local/bin/selenium-server-standalone.jar
 sudo chown root:root /usr/local/bin/selenium-server-standalone.jar
 sudo chmod 0755 /usr/local/bin/selenium-server-standalone.jar
-
-chromedriver --url-base=/wd/hub
