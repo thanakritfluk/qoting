6a6742f94643a28872dd868082616c56b0cb5098
Piyaphol Wiengperm <piyaphol.w@ku.th>
2018-11-29 13:11:53 +0700

    fixed selenium

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 sudo apt-get update
 sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
 
+CHROME_DRIVER_VERSION="2.41"
+SELENIUM_STANDALONE_VERSION="3.13.0"
+SELENIUM_SUBDIR="3.13"
+
 # Install Chrome.
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
